7f305de2acb3a984370e7f69d40ee2c6792edcc8
Michael A Maines <85315191+MMaines2005@users.noreply.github.com>
2022-02-01 08:43:49 -0500

    Update server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+    
+require("./config/mongoose.config");
+    
+app.use(express.json(), express.urlencoded({ extended: true }));
+    
+// app.use(require("./routes/food.routes"));
+    
+app.listen(8000, () => console.log("The server is all fired up on port 8000"));
