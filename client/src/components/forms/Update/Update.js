3bddda10cc23eb0782771c2fbab3f0ea9013539b
Miles Jiang <milesjiang18@gmail.com>
2022-02-02 22:08:45 -0500

    adding react-router package

--- client/src/components/forms/Update/Update.js
+++ client/src/components/forms/Update/Update.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
 
+//update is the shopping cart view
 const Update = () => {
   return (
     <div>
       UPDATE DETAILS PAGE
-      <Link to='/login'>login</Link> ||
-      <Link to='/home'>Home</Link>
+      <Link to="/login">login</Link> ||
+      <Link to="/home">Home</Link>
     </div>
   );
 };
